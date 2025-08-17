81f82b2a181b2924a441e03a5081c743c552add6
jpangi <jacob.Pangilinan10@gmail.com>
2025-08-17 12:28:23 -0700

    updated buttons to have draw card and play button, updated to draw computer and player card

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # project-1
 project-1 for Unit 1: Creating the Card game war
+
+# How to play WAR!:
+The deck is split evenly between two players.
+
+Each player flips the top card of their pile.
+
+The higher card wins, and that player takes both cards and puts them on the bottom of their pile.
+
+If the cards are equal, it’s a “war”: each player places 2  cards face down, then another face-up card. The higher face-up card wins all the cards.
+
+The game keeps going until one player has all the cards (they win).
+
+
--- app.js
+++ app.js
@@ -5,20 +5,7 @@ const deck = [
   { suits: ["heart", "diamond", "club", "spade"] },
   {
     ranks: [
-      "ace",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "jack",
-      "queen",
-      "king",
-    ],
+      "ace","2","3", "4","5","6", "7","8","9", "10","jack", "queen","king",],
   },
   { values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] },
 ];
@@ -30,21 +17,43 @@ const values = deck[2].values;
 let newDeck = [];
 let playerDeck = [];
 let computerDeck = [];
+let playerCard =[];
+let computerCard =[];
 
 /*----------------------- Element references -------------------*/
 const msgEl = document.querySelector("#msg");
-const playEl = document.querySelector("#play");
+const playEl = document.querySelector("#play-btn");
+const dealEl = document.querySelector("#deal-cards");
+const drawCardEl = document.querySelector("#draw-card");
+// display elements for deck size
 const computerEl = document.querySelector("#computer");
 const playerEl = document.querySelector("#player");
 
 /*----------------------- Functions ----------------------------*/
-const playGame = () => {
+
+// initializes the board, deck and updates the board/Message
+const init = () =>{
+  board
+}
+
+const dealCards = () => {
   createDeck();
   shuffleDeck(newDeck);
   // console.log(newDeck);
   splitDeck();
   deckSize();
 };
+
+const playGame = () =>{
+  drawCards();
+}
+
+const resetGame = () =>{
+
+}
+
+/* ------ Deck Functions ------ */
+
 
 // creates all the combination of suits and values for each card
 const createDeck = () => {
@@ -59,6 +68,7 @@ const createDeck = () => {
 };
 
 function shuffleDeck(deck) {
+  // fisher yates shuffle algorithm
   for (let i = deck.length - 1; i >= 1; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
@@ -69,26 +79,73 @@ function shuffleDeck(deck) {
 const splitDeck = () => {
   playerDeck = newDeck.slice(0, 26);
   computerDeck = newDeck.slice(26, 52);
-  // console.log(playerDeck);
-  // console.log(computerDeck);
+  console.log('Player Deck:',playerDeck);
+  console.log('Computer Deck:',computerDeck);
 };
 
+// updates what the deck shows on the dom for each player
 const deckSize = () => {
   playerEl.textContent = playerDeck.length;
   computerEl.textContent = computerDeck.length;
 };
 
-const gameLogic = () => {};
+
+/* ------- game logic function ------*/
+
+// draw cards
+const drawCards = () => {
+  playerCard = playerDeck.shift();
+  
+  console.log('Player Card:',playerCard);
+  console.log('Player Deck:',playerDeck);
+  computerCard = computerDeck.shift();
+  console.log('Computer Card:', computerCard);
+  console.log('Computer Deck:', computerDeck);
+  deckSize();
+}
+
+const simpleGameLogic = () => {
+  // if playerCard > compCard
+  
+  // add both card to bottom of playerdeck and subtract compCard from CompDeck
+
+};
+
+
+
+
 
 /*----------------------- Event Listeners ----------------------------*/
-playEl.addEventListener("click", playGame);
+dealEl.addEventListener("click", dealCards);
+// playEl.addEventListener("click", playGame);
+drawCardEl.addEventListener("click", playGame);
 
-// game logic
 
-// when you click on the card you update the board with the drawn card
 
-// compare the 2 cards
-// if one is bigger then they win
-// if tied draw 3 more cards and compare the 4th card
 
-// repeat until one card is bigger
+
+/* ----- guide ---- */
+
+
+
+// create a deck of cards
+// shuffles it
+
+
+// split the deck of cards in half
+  // show number of cards for each player on DOM
+  // update Message to say hit play button
+
+// press play
+// draws a card and updates it in the DOM(board)
+// if player > comp or comp > player give the 2 cards to whoever won
+  // update Message "player or comp WON round"
+  // adds the two cards to the back of the deck and subracts the card from the loser
+
+  // if cards are === value slice out 3 more cards and compare the 3rd card
+  // if player > comp or comp > player give the 2 cards to whoever won
+  // update Message "player or comp WON round"
+  // adds the all cards to the back of the deck and subracts the cards from the loser
+
+
+  // if player or computer has no more cards update message player or computer Wins Game
